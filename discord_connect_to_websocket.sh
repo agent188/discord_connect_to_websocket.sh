63255e4eca6eb3a88fbb6d8469a40d18107dd7e2
agent188 <41228098+agent188@users.noreply.github.com>
2021-06-22 10:11:37 +0300

    Update discord_connect_to_websocket.sh

--- discord_connect_to_websocket.sh
+++ discord_connect_to_websocket.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-randomPort=$(shuf -i 2000-65000 -n 1)
-echo "LOCALHOST LISTEN $randomPort PORT"
+touch /dev/shm/DiscordBot.txt
+chmod 700 /dev/shm/DiscordBot.txt
+echo -n > /dev/shm/DiscordBot.txt
 function HeartBeat()
 {
     while true
     do
         CheckPid
-        jo op=1 d=null > /dev/tcp/localhost/$randomPort
+        jo op=1 d=null >> /dev/shm/DiscordBot.txt
         sleep 1
     done
 }
@@ -25,6 +26,12 @@ function AsyncProcessing()
             ;;
     esac
 }
+function AuthenticatedTimeOut()
+{
+    sleep 5
+    echo "[$(date)] Authenticated timed out"
+    kill $pid
+}
 pid=$$
 bot=$(curl -sS -H "Authorization: Bot $tokenbot" "https://discord.com/api/gateway/bot")
 url=$(echo "$bot" | jq -r '.url')
@@ -32,55 +39,25 @@ if [[ ! $url || $url == 'null' ]]; then
     echo 'Failed get url'
     exit 1
 fi
-while true
+while read -r msg
 do
-    while read -r msg
-    do
-        if [[ $(echo "$msg" | jq -r '.t') == "READY" ]]; then
-            echo "[$(date)] Authorized as $(echo "$msg" | jq -r '.d.user.username')#$(echo "$msg" | jq -r '.d.user.discriminator')"
-            session_id=$(echo "$msg" | jq -r '.d.session_id')
-        fi
-        case $(echo "$msg" | jq -r '.op') in
-            0)
-                AsyncProcessing "$msg" &
-                sequence=$(echo "$msg" | jq -r '.s')
-                ;;
-            7)
-                echo "[$(date)] Reconnect..."
-                break
-                ;;
-            9)
-                echo "[$(date)] Invalid Session. Stopping bot..."
-                exit 1
-                ;;
-        esac
-        if [[ ! $heartbeat ]]; then
-            heartbeat=true
-            HeartBeat &
-            heartbeatpid=$!
-            continue
-        fi
-        if [[ $resume ]]; then
-            echo "[$(date)] Resume session"
-            jo op=6 d[token]="$tokenbot" d[session_id]="$session_id" d[seq]=$sequence > /dev/tcp/localhost/$randomPort
-            unset resume
-        fi
-        if [[ ! $auth ]]; then
-            echo "[$(date)] authenticated..."
-            echo '{ "op": 2, "d": { "token": "'$tokenbot'", "intents": 1026, "properties": { "$os": "linux", "$browser": "disco", "$device": "disco" }, "presence": { "activities": [ { "name": null, "type": 0 } ], "status": "online", "since": null, "afk": false } } }' > /dev/tcp/localhost/$randomPort
-            auth=true
-        fi
-    done < <(ncat -kl localhost $randomPort -m 10000000 | ./websocat "$url/?v=8&encoding=json" --ping-interval 1 --ping-timeout 2 -E -t)
-    kill $heartbeatpid
-    unset heartbeat
-    unset heartbeatpid
-    randomPort=$(shuf -i 2000-65000 -n 1)
-    echo "LOCALHOST LISTEN $randomPort PORT"
-    bot=$(curl -sS -H "Authorization: Bot $tokenbot" "https://discord.com/api/gateway/bot")
-    url=$(echo "$bot" | jq -r '.url')
-    if [[ ! $url || $url == 'null' ]]; then
-        echo 'Failed get url'
-        exit 1
+    if [[ $(echo "$msg" | jq -r '.t') == "READY" ]]; then
+        echo "[$(date)] Authorized as $(echo "$msg" | jq -r '.d.user.username')#$(echo "$msg" | jq -r '.d.user.discriminator')"
+        session_id=$(echo "$msg" | jq -r '.d.session_id')
+        kill $pidAuthenticatedTimeOut
+        unset pidAuthenticatedTimeOut
     fi
-    resume=true
-done
+    if [[ ! $heartbeat ]]; then
+        heartbeat=true
+        HeartBeat &
+        heartbeatpid=$!
+        continue
+    fi
+    if [[ ! $auth ]]; then
+        AuthenticatedTimeOut &
+        pidAuthenticatedTimeOut=$!
+        echo "[$(date)] authenticated..."
+        echo '{ "op": 2, "d": { "token": "'$tokenbot'", "intents": 1026, "properties": { "$os": "linux", "$browser": "disco", "$device": "disco" }, "presence": { "activities": [ { "name": null, "type": 0 } ], "status": "online", "since": null, "afk": false } } }' >> /dev/shm/DiscordBot.txt
+        auth=true
+    fi
+done < <(tail -n 0 -f --pid $pid /dev/shm/DiscordBot.txt | ./websocat "$url/?v=8&encoding=json" --ping-interval 5 --ping-timeout 10 -E -t)
